c06660c2be26ebe48d641b87f1d1e99c1510abd9
Akash <codeguyakash@gmail.com>
2024-05-29 23:09:44 +0530

    fix

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -28,7 +28,7 @@ const Dashboard = () => {
   ];
 
   const [page, setPage] = useState(0);
-  const [rowsPerPage, setRowsPerPage] = useState(5);
+  const [rowsPerPage, setRowsPerPage] = useState(10);
 
   const handleChangePage = (event, newPage) => {
     setPage(newPage);
@@ -40,42 +40,64 @@ const Dashboard = () => {
   };
 
   return (
-    <Container>
-      <Typography variant="h4" gutterBottom>
-        Dashboard
+    <>
+      <Container>
+        <Typography variant="h4" gutterBottom>
+          Admin Dashboard
+        </Typography>
+        <TableContainer component={Paper} id="table_container">
+          <Table>
+            <TableHead>
+              <TableRow>
+                {columns.map((column) => (
+                  <TableCell className="columns" key={column.id}>
+                    {column.label}
+                  </TableCell>
+                ))}
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {rows
+                .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                .map((row, index) => (
+                  <TableRow key={index}>
+                    {columns.map((column) => (
+                      <TableCell key={column.id}>{row[column.id]}</TableCell>
+                    ))}
+                  </TableRow>
+                ))}
+            </TableBody>
+          </Table>
+          <TablePagination
+            id="pagination"
+            rowsPerPageOptions={[10, 30, 40]}
+            component="div"
+            count={rows.length}
+            rowsPerPage={rowsPerPage}
+            page={page}
+            onPageChange={handleChangePage}
+            onRowsPerPageChange={handleChangeRowsPerPage}
+            sx={{
+              bgcolor: "primary.main",
+              color: "white",
+              "& .MuiTablePagination-toolbar": {
+                bgcolor: "primary.main",
+              },
+              "& .MuiTablePagination-selectLabel, & .MuiTablePagination-displayedRows":
+                {
+                  color: "white",
+                },
+              "& .MuiTablePagination-actions": {
+                color: "white",
+              },
+            }}
+          />
+        </TableContainer>
+      </Container>
+      <Typography variant="body2" align="center" mt={1}>
+        &copy; RMSI (Admin Dashboard) 2024
       </Typography>
-      <TableContainer component={Paper}>
-        <Table>
-          <TableHead>
-            <TableRow>
-              {columns.map((column) => (
-                <TableCell className="columns" key={column.id}>{column.label}</TableCell>
-              ))}
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {rows
-              .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-              .map((row, index) => (
-                <TableRow key={index}>
-                  {columns.map((column) => (
-                    <TableCell key={column.id}>{row[column.id]}</TableCell>
-                  ))}
-                </TableRow>
-              ))}
-          </TableBody>
-        </Table>
-        <TablePagination
-          rowsPerPageOptions={[5, 10, 25]}
-          component="div"
-          count={rows.length}
-          rowsPerPage={rowsPerPage}
-          page={page}
-          onPageChange={handleChangePage}
-          onRowsPerPageChange={handleChangeRowsPerPage}
-        />
-      </TableContainer>
-    </Container>
+    </>
   );
 };
 
